0fc4dec699cf573272372b60faf38b3a47ae7d52
Destyka <102505452+Destyka@users.noreply.github.com>
2022-08-04 14:06:24 +0200

    save exo

---
+++ src/actions/stable.action.js
@@ -0,0 +1,10 @@
+export const SET_STABLE_STATE = "SET_STABLE_STATE";
+
+export const setStableState = (bool) => {
+  return (dispatch) => {
+    dispatch({
+      type: SET_STABLE_STATE,
+      payload: bool,
+    });
+  };
+};
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import TableLine from "./TableLine";
 import ToTop from "./ToTop";
 
 const Table = ({ coinsData }) => {
   const [rangeNumber, setRangeNumber] = useState(100);
   const [orderBy, setOrderBy] = useState("");
+  const showStable = useSelector((state) => state.stableReducer);
+
   const tableHeader = [
     "Prix",
     "Market Cap",
@@ -17,6 +20,35 @@ const Table = ({ coinsData }) => {
     "1a",
     "ATH",
   ];
+
+  const excludeCoin = (coin) => {
+    if (
+      coin === "usdt" ||
+      coin === "usdc" ||
+      coin === "busd" ||
+      coin === "dai" ||
+      coin === "ust" ||
+      coin === "mim" ||
+      coin === "tusd" ||
+      coin === "usdp" ||
+      coin === "usdn" ||
+      coin === "fei" ||
+      coin === "tribe" ||
+      coin === "gusd" ||
+      coin === "frax" ||
+      coin === "lusd" ||
+      coin === "husd" ||
+      coin === "ousd" ||
+      coin === "xsgd" ||
+      coin === "usdx" ||
+      coin === "eurs"
+    ) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
   return (
     <div className="table-container">
       <ul className="table-header">
@@ -63,6 +95,15 @@ const Table = ({ coinsData }) => {
         coinsData
           .slice(0, rangeNumber)
           // eslint-disable-next-line array-callback-return
+          .filter((coin) => {
+            if (showStable) {
+              return coin;
+            } else {
+              if (excludeCoin(coin.symbol)) {
+                return coin;
+              }
+            }
+          })
           .sort((a, b) => {
             switch (orderBy) {
               case "Prix":
